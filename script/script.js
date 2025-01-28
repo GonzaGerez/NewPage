fb691cbc00b5ccb00666739ee7e610a104fa6ea3
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2025-01-28 15:35:11 +0100

    sumando clases de js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,36 @@
-const saludo = ("Hola")
-/* console.log (saludo)
-document.getElementById("mensaje").textContent = saludo; */
+const titulo = ("bienvenido")
+
+console.log(titulo)
+document.getElementById("prueba").textContent = titulo;
+
+
+/*
+{
+    let nombre =("Pedro")
+    console.log(nombre)
+}
+
+console.log(nombre)
+
+/*let apellido = ("Perez");
+let dni = 39123113;
+
+console.log(nombre,apellido,dni);
+
+let persona  = ["Juan","Perez",39123113];
+
+console.log(persona);
+
+/*
+
+
+
+let saludo = ("Hola")
+if(true){
+    let saludo = ("Hola2")
+
+    console.log (saludo)
+}
+console.log (saludo)
+document.getElementById("mensaje").textContent = saludo;
+*/
